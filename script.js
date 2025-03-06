13b88769d85c17e92ea515995982f17c04e7680c
sohaibyounas <sohaibyounas24@gmail.com>
2025-03-06 16:03:46 +0500

    minor changes

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scrolled');
   }
 });
+
+
+// // play button animation
+// document.addEventListener("DOMContentLoaded", () => {
+//   const elements = document.querySelectorAll(".animate-on-scroll");
+
+//   const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("active");
+//         observer.unobserve(entry.target); // Stop observing after animation
+//       }
+//     });
+//   }, {
+//     threshold: 0.5 // Trigger when 50% of the element is visible
+//   });
+
+//   elements.forEach(element => observer.observe(element));
+// });
