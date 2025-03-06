09b77bbd9f8acd85200be2f70338fdfe5ebd5b8a
sohaibyounas <sohaibyounas24@gmail.com>
2025-03-06 10:51:57 +0500

    nav scroll

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function headernav() {
 }
 
 
-// nav bg-color on scroll 
-window.onscroll = function() {
-    let mheader = document.getElementById("main-header");
-    if (window.scrollY > 10){
-        header.classList.add('scrolled')
-    }else{
-        header.classList.remove('scrolled')
-    };
-};
+// navbar color changes on scroll
+window.addEventListener('scroll', function() {
+  let header = document.getElementById('main-header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
